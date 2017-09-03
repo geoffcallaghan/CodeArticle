5d4d9a6d3362dcac4c2f083c52be648f30a557b1
geoffcallaghan <geoffcallaghan1@gmail.com>
2017-09-03 14:24:53 -0600

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
 	{
 		private static void Main(string[] args)
 		{
-			Console.WriteLine("G'day Mate!");
+			Console.WriteLine("Hello World!");
 			Console.ReadLine();
 		}
 	}
