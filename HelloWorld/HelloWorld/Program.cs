f587d299fc53c023d0359a9c0926eac833a95647
gcallaghan <geoffcallaghan1@gmail.com>
2017-09-03 13:55:17 -0600

    Changed to Australian

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
 	{
 		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("G'day Mate!");
 			Console.ReadLine();
 		}
 	}
