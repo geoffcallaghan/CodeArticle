139388f7a3c9e94ab66ee51c35cd7c300dbff640
gcallaghan <geoffcallaghan1@gmail.com>
2017-09-04 18:04:44 -0600

    Added a language translator for greetings

---
+++ HelloWorld/Helpers/GreetingsHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace Helpers
+{
+	public static class GreetingsHelper
+	{
+		public static string Greeting(Language language)
+		{
+			switch (language)
+			{
+				case Language.American:
+				return "Hello World";
+
+				case Language.Australian:
+				return "G'date Mate!";
+
+				case Language.Spanish:
+				return "Hola!";
+
+				default:
+				return "Unknown language!";
+			}
+		}
+	}
+
+	public enum Language
+	{
+		American,
+		Australian,
+		Spanish
+	}
+}
